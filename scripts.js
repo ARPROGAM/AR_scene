74781d39686c272c31b10fee8bb63960c566e00a
ARPROGAM <167599005+ARPROGAM@users.noreply.github.com>
2024-09-16 11:08:33 +0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,4 +0,0 @@
-function changeModel() {
-    const modelEntity = document.querySelector('a-entity[gltf-model]');
-    modelEntity.setAttribute('gltf-model', 'https://new-link-to-your-second-model.glb');
-}
